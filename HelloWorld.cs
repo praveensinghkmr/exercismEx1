27265e19b6b25f28ec7f5a5084b55e721c5b9d44
Praveenkumar Singh <praveenkumar.singh@in.abb.com>
2018-09-19 18:39:13 +0530

    First Commit

---
+++ HelloWorld.cs
@@ -0,0 +1,10 @@
+using System;
+
+public static class HelloWorld
+{
+    public static string Hello()
+    {
+        return "Hello, World!";
+        //throw new NotImplementedException("You need to implement this function.");
+    }
+}
